d1144a20cc13125cd5c8b184b25a73ddbe16ad81
Jithin K Tom <X195046@ap.merckgroup.com>
2020-05-07 14:55:48 +0530

    Dummy data - working model

--- README.md
+++ README.md
@@ -19,4 +19,15 @@ Request successful - 1. invalid session 2. valid session
 Request failed
 
 ### Things to do
+Integrate with websocket
+Add new series dynamically
+Optionally hide and show series.
+Change chart design to area/line
+Change to stacked/overlay
+
+## Questions to Pradeeban
+If I subscribe to a topic of websocket, will it be the same type of data coming everytime? 
+Should we dynamically add series to chart? Or will it be pre-defined?
+
+
 
--- src/components/ChartContainer/Chart.js
+++ src/components/ChartContainer/Chart.js
@@ -1,15 +1,18 @@
 import React from "react";
+// import Highcharts from "highcharts";
 import Highcharts from "highcharts/highstock";
 import HighchartsReact from "highcharts-react-official";
 import { Client } from '@stomp/stompjs';
 
 import HC_more from "highcharts/highcharts-more"; //module
+import accessibility from "highcharts/modules/accessibility"; //module
 HC_more(Highcharts); //init module
 
 class Chart extends React.Component {
     constructor(props) {
         super(props);
-        console.log(props.chartData)
+        this.chartComponent = React.createRef();
+        var classComponent = this;
 
         this.state = {
             options: {
@@ -17,14 +20,37 @@ class Chart extends React.Component {
                     events: {
                         load: function () {
 
-                            // set up the updating of the chart each second
-                            var series = this.series[0];
-
+                            var xAxis = this.xAxis[0]
+                            var chart = this;
+                            const seriesArr = ["Temperature", "pH", "Pressure"];
+                            var series0 = this.series[0]
+                            var series1 = this.series[1]
+                            
                             setInterval(function () {
+                                // console.log(this)
                                 var x = (new Date()).getTime(), // current time
-                                    y = Math.round(Math.random() * 100);
-                                series.addPoint([x, y], true, true);
+                                    y = Math.floor(Math.random() * 50);
+
+                                    var a = (new Date()).getTime(), // current time
+                                    b = Math.floor(Math.random() * 50);
+                                // classComponent.appendData({
+                                //     "name": seriesArr[Math.floor(Math.random() * seriesArr.length)],
+                                //     data: [x, y]
+                                // })
+
+                                // var setExtremes = xAxis.max == xAxis.dataMax && this.fixedRange;
+                                // series0.addPoint([x, y], true, false);
+                                series1.addPoint([a, b], true, false);
+
+                                // if (setExtremes) {
+                                //     xAxis.setExtremes(x - this.fixedRange, x);
+                                // }
+                                // chart.redraw();
+
+                                xAxis.setExtremes(x - 1000 * 60, x)
                             }, 1000);
+
+
 
                             // this.client = new Client();
                             // this.client.configure({
@@ -66,32 +92,106 @@ class Chart extends React.Component {
                 },
 
                 title: {
-                    text: props.chartData.name
+                    text: "OPC UA Data"
                 },
 
                 exporting: {
-                    enabled: true
+                    enabled: false
+
+                },
+                xAxis: {
+                    type: "datetime"
                 },
 
-                series: [{
-                    name: props.chartData.name,
-                    data: props.chartData.data,
-                    type: "spline",
-                    lineColor: '#ff1e00',
-                    color: '#f2dcfa'
-                }]
+                legend: {
+                    layout: 'vertical',
+                    verticalAlign: 'middle',
+                    align: 'left',
+                    showCheckbox: true,
+                    enabled: true,
+                    itemCheckboxStyle: { "width": "13px", "height": "13px", "position": "absolute" },
+                    title: {
+                        style: { "fontWeight": 100, "fontFamily": '"verdana", Arial, sans-serif' }
+                    }
+                },
+
+                series: [
+                        {
+                        name: props.chartData.name,
+                        data: props.chartData.data,
+                        type: "spline",
+                        lineColor: '#ff1e00',
+                        color: '#ff1e00'
+                    },
+                    {
+                        name: props.chartData.name + "_copy",
+                        data: props.chartData.data,
+                        type: "spline",
+                        lineColor: 'green',
+                        color: 'green'
+                    }
+                    // {
+                    //     data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+                    // },
+                    // {
+                    //     data: [59.9, 71.5, 6.4, 129.2, 64.0, 176.0, 75.6, 148.5, 286.4, 64.1, 6.6, 34.4]
+                    // }
+                ]
             }
         };
+    }
+    appendData = (data) => {
+        let chart = this.chartComponent.current.chart;
+        // var series = false;
+        // console.log(chart.series[0])
+        chart.series[1].addPoint(Math.floor(Math.random() * 40), true);
+        // chart.redraw()
+        // for (var i = 0; i < chart.series.length; i++) {
+        //     if (chart.series[i].name === data.name) {
+        //         series = chart.series[i];
+        //     }
+        // }
+
+
+        // // console.log(series.addPoint)
+        // if (series) {
+        //     console.log(series.name, series.index)
+        //     series.addPoint(data.data, true, true);
+        // }
+        // else {
+        //     chart.addSeries({
+        //         name: data.name,
+        //         data: [data.data]
+        //     });
+        // }
+
+    }
+    handleClick = () => {
+        console.log("button clicked");
+        console.log(this.chartComponent.current.chart)
+        this.chartComponent.current.chart.addSeries({
+            name: "pH",
+            data: (function () {
+                var arr = []
+                for (let i = 0; i < 30; i++) {
+                    arr.push([Date.now() + Math.floor(Math.random() * Math.floor(4000)), Math.floor(Math.random() * Math.floor(40))])
+                }
+                return arr
+            }())
+        });
     }
 
     render() {
         return (
-            <HighchartsReact
-                constructorType={"stockChart"}
-                ref={this.chartComponent}
-                highcharts={Highcharts}
-                options={this.state.options}
-            />
+            <div>
+                <HighchartsReact
+                    constructorType={"stockChart"}
+                    ref={this.chartComponent}
+                    highcharts={Highcharts}
+                    options={this.state.options}
+                />
+                <button onClick={this.handleClick}>Add series</button>
+            </div>
         );
     }
 }
